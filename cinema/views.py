837478d94804a7e9d42524faa0a725f466ad0adc
Yura Chornyi <chorny.yura@gmail.com>
2025-03-24 21:56:22 +0200

    feat: add filtering for movie list view

--- cinema/views.py
+++ cinema/views.py
@@ -33,8 +33,27 @@ class CinemaHallViewSet(viewsets.ModelViewSet):
 
 
 class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all()
+    queryset = Movie.objects.prefetch_related("actors", "genres")
     serializer_class = MovieSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action == "list":
+            actors = self.request.query_params.get("actors", None)
+            genres = self.request.query_params.get("genres", None)
+            title = self.request.query_params.get("title", None)
+
+            if actors:
+                actors = [int(num) for num in actors.split(",")]
+                queryset = self.queryset.filter(actors__id__in=actors)
+            if genres:
+                genres = [int(num) for num in genres.split(",")]
+                queryset = self.queryset.filter(genres__id__in=genres)
+            if title:
+                queryset = self.queryset.filter(title__icontains=title)
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -49,6 +68,10 @@ class MovieViewSet(viewsets.ModelViewSet):
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
     serializer_class = MovieSessionSerializer
+
+    @staticmethod
+    def _param_to_id_list(string: str) -> list:
+        return [int(num) for num in string.split(",")]
 
     def get_serializer_class(self):
         if self.action == "list":
